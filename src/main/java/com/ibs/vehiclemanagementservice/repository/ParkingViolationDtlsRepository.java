318f263cf224e8ce8dabfaaff976bd9e3b6bac78
Aishwarya Rajaratnam <v-airajaratnam@expedia.com>
2020-05-20 00:46:31 +0530

    parking violation user view

--- src/main/java/com/ibs/vehiclemanagementservice/repository/ParkingViolationDtlsRepository.java
+++ src/main/java/com/ibs/vehiclemanagementservice/repository/ParkingViolationDtlsRepository.java
@@ -3,11 +3,13 @@ package com.ibs.vehiclemanagementservice.repository;
 import com.ibs.vehiclemanagementservice.model.ParkingViolation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import java.util.List;
+
 
 @Repository
 public interface ParkingViolationDtlsRepository extends MongoRepository<ParkingViolation, Integer> {
 
-    Optional<ParkingViolation> findByEmployeeId(String empId);
+    List<ParkingViolation> findByEmployeeId(String empId);
 
 }
