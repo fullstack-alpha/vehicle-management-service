30b656c3bafb3cce10a65c19e8f78a57a38b8950
Aishwarya Rajaratnam <v-airajaratnam@expedia.com>
2020-05-21 17:22:31 +0530

    Added delete button for parking violation records

--- src/main/java/com/ibs/vehiclemanagementservice/repository/ParkingViolationDtlsRepository.java
+++ src/main/java/com/ibs/vehiclemanagementservice/repository/ParkingViolationDtlsRepository.java
@@ -11,5 +11,6 @@ import java.util.List;
 public interface ParkingViolationDtlsRepository extends MongoRepository<ParkingViolation, Integer> {
 
     List<ParkingViolation> findByEmployeeId(String empId);
+    void deleteByViolationId(int id);
 
 }
